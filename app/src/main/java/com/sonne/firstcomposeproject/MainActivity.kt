754038c0f82d09b60c4b14e04b112c9440e9b023
sonne <sonnne34@gmil.com>
2022-12-02 15:23:10 +0500

    done

--- app/src/main/java/com/sonne/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sonne/firstcomposeproject/MainActivity.kt
@@ -3,31 +3,56 @@ package com.sonne.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserInfo(name = "Настя", age = 31)
         }
     }
 }
 
 @Preview
 @Composable
-fun UserInfoPreview(){
-    UserInfo(name = "Настя", age = 31)
-}
-
-@Composable
-fun UserInfo(name: String, age: Int) {
-    Column {
-        repeat(10) {
-            Text(text = "Привет, $name! Тебе $age лет.")
+fun TimesTable() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        for (i in 1..9) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1..9) {
+                    val color = if ((i + j) % 2 == 0) {
+                        Color.Yellow
+                    } else {
+                        Color.White
+                    }
+                    Box(
+                        modifier = Modifier
+                            .background(color)
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(1.dp, color = Color.DarkGray),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "${j * i}")
+                    }
+                }
+            }
         }
     }
 }
