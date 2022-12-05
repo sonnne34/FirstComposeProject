4299f25503a543ef5ea3fa6dae935b4b2373173f
sonne <sonnne34@gmil.com>
2022-12-05 10:55:48 +0500

    done

--- app/src/main/java/com/sonne/firstcomposeproject/ui/InstagramProfile.kt
+++ app/src/main/java/com/sonne/firstcomposeproject/ui/InstagramProfile.kt
@@ -2,7 +2,6 @@ package com.sonne.firstcomposeproject.ui
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
